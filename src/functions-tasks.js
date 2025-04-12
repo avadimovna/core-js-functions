76b62966e38a3b878cac846c38be1b0ce6bae90c
avadimovna <anastasia.khotyan@gmail.com>
2025-04-12 13:26:45 +0300

    feat: complete rest functions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -215,8 +215,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function myFunc(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -236,8 +238,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let cache = startFrom;
+  return function myFunc() {
+    const result = cache;
+    cache += 1;
+    return result;
+  };
 }
 
 module.exports = {
